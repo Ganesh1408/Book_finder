0d814a681df7cf468c5545cf83156c5c42617273
Ganesh1408 <121733704+Ganesh1408@users.noreply.github.com>
2025-08-27 15:26:37 +0530

    add margin to elemtns

--- src/Components/LandingPage/index.jsx
+++ src/Components/LandingPage/index.jsx
@@ -4,13 +4,13 @@ function LandingPage() {
   return (
     <div className="h-screen w-screen">
       {/* <h3 className="pl-42 pt-10 text-2xl font-bold decoration-green-500">ReadNow</h3> */}
-      <span className="ml-26 sm:ml-32 pt-10 md:ml-40 font-bold text-2xl inline-block relative after:content-[''] after:block after:absolute after:left-0 after:bottom-0 after:w-[50%] after:h-1 after:bg-teal-500">
+      <span className="ml-22 sm:ml-32 pt-10 md:ml-40 font-bold text-2xl inline-block relative after:content-[''] after:block after:absolute after:left-0 after:bottom-0 after:w-[50%] after:h-1 after:bg-teal-500">
         ReadNow
       </span>
       <div className=" flex flex-col sm:flex-col md:flex-row  justify-evenly items-center ">
         <div className="  overflow-hidden flex flex-col justify-center items-center order-2 sm:order-2 md:order-1">
           <div className="w-full max-w-[70%] sm:max-w-[70%] md:max-w-[60%] ">
-            <h2 className="font-mono text-2xl sm:text-2xl md:text-3xl font-bold">
+            <h2 className="font-mono mt-4 sm:mt-4 md:mt-0 text-2xl sm:text-2xl md:text-3xl font-bold">
               <span className="text-teal-500">Every Book</span> You Want, One
               Search Away
             </h2>
@@ -33,7 +33,7 @@ function LandingPage() {
         <img
           src="/book.jpg"
           alt="landing_page_img"
-          className="w-[420px] h-[320px] order-1  sm:order-1 md:order-2 sm:w-[550px] sm:h-[420px]  md:w-[700px] md:h-[600px] md:relative md:top-10 md:mr-30 "
+          className="w-[420px] h-[320px] order-1 mt-4 sm:mt-4 md:mt-0  sm:order-1 md:order-2 sm:w-[550px] sm:h-[420px]  md:w-[700px] md:h-[600px] md:relative md:top-10 md:mr-30 "
         />
       </div>
     </div>
